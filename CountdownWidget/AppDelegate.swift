9b6ed1ef4a5a214759a331220f76f40fc76a03ea
lizdo <lizdourden@gmail.com>
2015-03-01 09:41:10 +0800

    Initial Commit

---
+++ CountdownWidget/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CountdownWidget
+//
+//  Created by Liz on 15/3/1.
+//  Copyright (c) 2015年 Liz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
